98ffd4af1b1117958cff2db1f8333ebaf6ee26f8
kyunyoung000 <kyunyoung@sookmyung.ac.kr>
2025-10-05 21:31:49 +0900

    05-5 수정

--- main.c
+++ main.c
@@ -3,26 +3,36 @@
 
 int main(int argc, char *argv[])
 { 
-    int answer = 59;
-    int num;
-    int trial=0;
+    int a, b;
+    char op;
+    int result;
         
-   do
-   {
-        printf("input a number:");
-        scanf("%d", &num);
-        
-        if (answer < num)
-           printf("high!\n");
-        else if (answer > num)
-           printf("low!\n");
-           
-        trial++;
+    printf("enter the calculation:");
+    scanf("%i %c %i", &a, &op, &b);
+    
+    switch(op)
+    {
+              case '+':
+                   result = a + b;
+                   break;
+                   
+              case '-': 
+                   result = a - b;
+                   break;
+                   
+              case '*': 
+                   result = a * b;
+                   break;
+                   
+              case '/': 
+                   result = a / b;
+                   break;
+              default:
+                   break;
     }
+    printf("=%i\n", result);
     
-    while(answer != num);
     
-    printf("Congratulation! trial:%i\n", trial);
         
     system("PAUSE");
     return 0;
